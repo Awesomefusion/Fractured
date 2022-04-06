3c9f8af219074dd03cd7f00565c4161d2df9551d
Awesomefusion <alexmould@hotmail.co.uk>
2022-04-06 14:33:22 +0100

    Skill class added

---
+++ Domain/Skill.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Skill
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string School { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Requirements { get; set; }
+        public string Cooldown { get; set; }
+    }
+}
