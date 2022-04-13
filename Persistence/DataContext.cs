4f5273b5358bd2a1840427384fa0413a04c5b0ac
Awesomefusion <alexmould@hotmail.co.uk>
2022-04-14 00:43:16 +0100

    Building EntityFramework DB

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace Persistence
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+         public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Skill> Skills { get; set; }
     }
 }
