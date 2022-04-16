328b5aa0912bd16c259474f75b66fa1ccb74238b
Awesomefusion <alexmould@hotmail.co.uk>
2022-04-16 17:40:56 +0100

    Database seed data added for testing

---
+++ Persistence/Seed.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Activities.Any()) return;
+
+            var skills = new List<Skill>
+            {
+                new Skill
+                {
+                    Title = "Absorb Elements",
+                    School = "Abjuration",
+                    Description = "You surround yourself with a magical protection that absorbs [INT*2]% of all the Fire/Ice/Shock damage you receive, half of which is then converted into Mana.",
+                    Image = "../assets/icons/Icon_Absorb.webp",
+                    Requirements = "Allowed Weapons: Spell Channeling, Allowed Armor: Light, Mana Cost: 300 + (30/s)",
+                    Cooldown = "10s",
+                },
+                new Skill
+                {
+                    Title = "Dispel Magic",
+                    School = "Abjuration",
+                    Description = "You attempt to dispel all magical buffs, debuffs and protections from all the creatures within a 3-meter radius area of your choice. For each effect, you have a [INT*3]% chance to dispel it, but the recipient is allowed a Willpower Save to keep it if it was a buff or a protection.",
+                    Image = "../assets/icons/Icon_DispelMagic.webp",
+                    Requirements = "Allowed Weapons: Spell Channeling, Allowed Armor: Any, Mana Cost: 400",
+                    Cooldown = "10s",
+                },
+                new Skill
+                {
+                    Title = "Globe of Spell Protection",
+                    School = "Abjuration",
+                    Description = "This globe completely blocks the effects of any single-targeted spell.",
+                    Image = "../assets/icons/Icon_GlobeOfSpellProtection.webp",
+                    Requirements = "Allowed Weapons: Unarmed Spell Channeling, Allowed Armor: Any, Mana Cost: 300 + (30/s)",
+                    Cooldown = "10s",
+                },
+            };
+
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
