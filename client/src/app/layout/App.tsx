84192e02cddc69a792e6445e1d79e2176e3b4c69
Awesomefusion <alexmould@hotmail.co.uk>
2022-04-25 15:56:26 +0100

    Minor refactoring of component tree

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CssBaseline from '@mui/material/CssBaseline';
-import Image from 'material-ui-image'
+import React from 'react';
+import ContentSwtich from '../components/ContentSwitch';
 
 function App() {
-  const [skills, setActivites] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/skills').then(response => {
-      setActivites(response.data);
-    })
-  }, [])
 
 
   return (
-    <div>
-      <ul>
-        {skills.map((skill: any) =>(
-          <li key={skill.Id}>
-            {skill.title}
-            <img src={skill.image}/>
-          </li>
-        ))}
-      </ul>
-      <CssBaseline/>
-    </div>
+    <ContentSwtich/>
   );
 }
-
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,11 +5,13 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
 );
