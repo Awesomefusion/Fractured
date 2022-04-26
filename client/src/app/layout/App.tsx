ea18a107e1555a5efd46ef47d023141d92a14e3d
Awesomefusion <alexmould@hotmail.co.uk>
2022-04-26 05:05:30 +0100

    implemented reactive navbar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,17 @@
+import Grid from '@mui/material/Grid';
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Navbar from '../components/Navbar';
+
 
 function App() {
 
-
   return (
-      <Navbar/>
-      
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Navbar/>
+      </Grid>
+    </Grid>
   );
 }
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/layout/App';
+import App from '../src/app/layout/App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
