17b8964fff642203b5e41a5808e79cd9fb8877ad
Awesomefusion <alexmould@hotmail.co.uk>
2022-04-29 00:20:20 +0100

    axios base code implemented

--- client/src/app/stores/skillStore.ts
+++ client/src/app/stores/skillStore.ts
@@ -1,8 +1,22 @@
 import { makeAutoObservable, observable } from 'mobx';
 import { makeObservable } from 'mobx';
+import { Skill } from '../models/skill';
 export default class SkillStore {
+
+    skills: Skill[] = [];
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
+    }
+
+    loadSkills = async () => {
+        this.loadingInitial = true;
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
